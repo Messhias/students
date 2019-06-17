0e8074126bb90b03ee91be72cedcbb08e28985c3
Fabio William Conceição <messhias@gmail.com>
2019-06-17 17:31:02 +0400

    framework core class done

---
+++ application/controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by fabioconceicao.
+ * FILE: Controller.php
+ * User: fabioconceicao
+ * Date: 2019-06-17
+ * Time: 17:30
+ */
+
+/**
+ * Class Controller
+ *
+ * Base controller method to be in the application 
+ */
+class Controller
+{
+
+}
