b7d8eb0e827bc3eebbfe60b537646c97685edaa1
Fabio William Conceição <messhias@gmail.com>
2019-06-17 16:44:20 +0400

    created the basic application

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Fabio William Conceição.
+ * @FILE: index.php
+ * @Date: 2019-06-17
+ * @Time: 16:42
+ *
+ * All the copyrights of this is under open source free license.
+ *
+ * This framework, files and any data of this application can be used without any charges and limitation.
+ *
+ */
