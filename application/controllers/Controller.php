626a6bbfc12867669d015d98645514cb39369b11
Fabio William Conceição <messhias@gmail.com>
2019-06-17 17:33:07 +0400

    base controller class done

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -10,9 +10,24 @@
 /**
  * Class Controller
  *
- * Base controller method to be in the application 
+ * Base controller method to be in the application
  */
 class Controller
 {
+    // Base Controller has a property called $loader, it is an instance of Loader class(introduced later)
+    protected $loader;
 
+    public function __construct(){
+        $this->loader = new Loader();
+    }
+
+    public function redirect($url, $message , $wait = 0){
+        if ($wait == 0){
+            header("Location:$url");
+        } else {
+            include CURR_VIEW_PATH . "message.html";
+        }
+        exit;
+
+    }
 }
