a48fba64187e1e7312b77f9a43adedaffae70783
Fabio William Conceição <messhias@gmail.com>
2019-06-17 18:36:03 +0400

    Created the controllers framework core class Created the view framework core class Created the database framework core class -> still only supports mysql Created the router framework core class. Created the Error default handlers for framework core classes and support the application.

---
+++ core/View.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * @author Fabio William Conceição
+ */
+
+// class namespace.
+namespace Core;
+
+// importing the uses statements
+use Exception;
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Error\SyntaxError;
+use Twig\Loader\FilesystemLoader;
+
+/**
+ * View
+ *
+ * In this class we set up the views and render for the ender user.
+ *
+ * PHP version 7.0
+ */
+class View
+{
+    /**
+     * Render a view file
+     *
+     * @param string $view
+     * @param array  $args
+     *
+     * @return void
+     * @throws Exception
+     */
+    public static function render($view, $args = [])
+    {
+        extract($args, EXTR_SKIP);
+
+        $file = dirname(__DIR__) . "/app/Views/$view";  // relative to core directory
+
+        if (is_readable($file)) {
+            /**
+             * Linter bypass
+             * @noinspection PhpIncludeInspection
+             */
+            require $file;
+        }
+        else {
+            throw new Exception("$file not found");
+        }
+    }
+
+    /**
+     * Render a view template using Twig
+     *
+     * @param string $template
+     * @param array $args
+     *
+     * @return void
+     */
+    public static function renderTemplate($template, $args = [])
+    {
+        static $twig = null;
+
+        if ($twig === null) {
+            $loader = new FilesystemLoader(dirname(__DIR__) . '/app/Views');
+            $twig = new Environment($loader);
+        }
+
+        try {
+            echo $twig->render($template, $args);
+        } catch (LoaderError $e) {
+            echo $e->getMessage();
+        } catch (RuntimeError $e) {
+            echo $e->getMessage();
+        } catch (SyntaxError $e) {
+            echo $e->getMessage();
+        }
+    }
+}
